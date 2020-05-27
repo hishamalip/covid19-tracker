668674cc8ac5f74bedc387cd8e2c204248ecd78f
hishamalip <hishamalip@gmail.com>
2020-05-28 03:51:01 +0530

    components added

---
+++ src/Components/Header.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+// importing components from react bootstrap
+import { Navbar, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Link to="/" className="navbar-brand">Covid-19 Tracker</Link>
+                {/* <Navbar.Brand href="#home"></Navbar.Brand> */}
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Link className="nav-link" to="/">India</Link>
+                        <Link className="nav-link" to="/world">World</Link>
+                        <Link className="nav-link" to="/kerala">Kerala</Link>
+                        {/* <Nav.Link href="/">India</Nav.Link>
+                        <Nav.Link href="/world">World</Nav.Link> */}
+                    </Nav>
+                    <Nav>
+                        <Nav.Link href="#deets">More deets</Nav.Link>
+                        <Nav.Link eventKey={2} href="#memes">
+                            Dank memes
+                            </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        );
+    }
+}
+
+export default Header;
---
+++ src/Components/Kerala.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Kerala extends React.Component{
+    render(){
+        return(
+            <div>
+                Keralite Component
+            </div>
+        );
+    }
+}
+
+export default Kerala;
