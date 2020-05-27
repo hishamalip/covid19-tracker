6369dfa0a19b4d705f20f4f37f894f660e0f9fd5
hishamalip <hishamalip@gmail.com>
2020-05-28 03:51:55 +0530

    Routing implemented

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Header from './Components/Header';
+import India from './Components/India';
+import Kerala from './Components/Kerala';
+import World from './Components/World';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <BrowserRouter>
+      <div className="container-fluid">
+        <Header />
+      </div>
+
+      <Switch>
+        <Route exact path="/">
+          <India />
+        </Route>
+
+        <Route path="/india">
+          <India />
+        </Route>
+
+        <Route path="/kerala">
+          <Kerala />
+        </Route>
+    
+        <Route path="/world">
+          <World />
+        </Route>
+      </Switch>
+
+    </BrowserRouter>
+
   );
 }
 
