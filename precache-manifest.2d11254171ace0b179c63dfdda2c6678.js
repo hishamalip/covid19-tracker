7db3f4f40bc71c870f4334f8ca3a75f289ce30bd
hishamalip <hishamalip@gmail.com>
2020-05-30 00:36:20 +0530

    Updates

---
+++ precache-manifest.2d11254171ace0b179c63dfdda2c6678.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8d47e5f48141c79837e7fa9f929c332",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "fc9b242f9b31b5ee1c4f",
+    "url": "/covid19-tracker/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c940c46b78deac3b3856",
+    "url": "/covid19-tracker/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fc9b242f9b31b5ee1c4f",
+    "url": "/covid19-tracker/static/js/2.3fcecbb2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/covid19-tracker/static/js/2.3fcecbb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c940c46b78deac3b3856",
+    "url": "/covid19-tracker/static/js/main.c7d5cd7b.chunk.js"
+  },
+  {
+    "revision": "8cd54aa8f9c07f9dab41",
+    "url": "/covid19-tracker/static/js/runtime-main.c8a8ed36.js"
+  }
+]);
