ee96c908e8373b639f469ca60a31d3715e7e9375
hishamalip <hishamalip@gmail.com>
2020-06-05 23:37:08 +0530

    table text centered

--- src/Components/Kerala.js
+++ src/Components/Kerala.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import { Container, Row, Col } from 'react-bootstrap';
-import { Accordion, Card } from 'react-bootstrap';
-import { Table, Badge, Button } from 'react-bootstrap';
+import { Table } from 'react-bootstrap';
 
 import KeralaCards from './KeralaCards';
 
@@ -25,59 +24,57 @@ class Kerala extends React.Component {
     render() {
         var district_data = this.state.district_data;
         var district_names = Object.keys(this.state.district_data);
-        let table_data = district_names.map( (item, index) => {
+        let table_data = district_names.map((item, index) => {
             return (
                 <tr key={index}>
                     <td>{index + 1}</td>
                     <td className="table-active">{item}</td>
-                    <td className="table-primary">{ district_data[item].confirmed }</td>
-                    <td className="">{ district_data[item].delta.confirmed }</td>
-                    <td className="table-warning">{ district_data[item].active }</td>
-                    <td className="table-danger">{ district_data[item].deceased }</td>
-                    <td>{ district_data[item].delta.deceased }</td>
-                    <td className="table-success">{ district_data[item].recovered }</td>
-                    <td>{ district_data[item].delta.recovered}</td>
+                    <td className="table-primary text-center">{district_data[item].confirmed}</td>
+                    <td className="table-warning text-center">{district_data[item].delta.confirmed}</td>
+                    <td className="text-center">{district_data[item].active}</td>
+                    <td className="table-danger text-center">{district_data[item].deceased}</td>
+                    <td className="text-center">{district_data[item].delta.deceased}</td>
+                    <td className="table-success text-center">{district_data[item].recovered}</td>
+                    <td className="text-center">{district_data[item].delta.recovered}</td>
                 </tr>
             );
         })
-        
+
         return (
             <Container fluid>
-                    <Row>
-                        <Col>
-                            <KeralaCards />
-                        </Col>
+                <Row>
+                    <Col>
+                        <KeralaCards />
+                    </Col>
+                </Row>
+
+                <Row>
+                    &nbsp;
                     </Row>
 
-                    <Row>
-                        &nbsp;
-                    </Row>
-
-                    <Row>
-                        <Col>
-                            <Table responsive bordered hover size="sm">
-                                <thead className="thead-dark">
-                                    <tr>
-                                        <th>#</th>
-                                        <th>District / Other</th>
-                                        <th>Cases</th>
-                                        <th>Today Cases</th>
-                                        <th>Active</th>
-                                        <th>Deaths</th>
-                                        <th>Today Deaths</th>
-                                        <th>Recoved</th> 
-                                        <th>Today Recovered</th>
-                                        <th></th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {table_data}
-                                </tbody>
-                            </Table>
-                        </Col>
-                    </Row>
-
-                </Container>
+                <Row>
+                    <Col>
+                        <Table responsive bordered hover text-center size="sm">
+                            <thead className="thead-dark">
+                                <tr>
+                                    <th>#</th>
+                                    <th>District / Other</th>
+                                    <th className="bg-primary text-center">Total Cases</th>
+                                    <th className="bg-warning text-center">New Cases</th>
+                                    <th className="text-center">Active Cases</th>
+                                    <th className="bg-danger text-center">Deaths</th>
+                                    <th className="text-center">New Deaths</th>
+                                    <th className="bg-success text-center">Recoved</th>
+                                    <th className="text-center">New Recovered</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {table_data}
+                            </tbody>
+                        </Table>
+                    </Col>
+                </Row>
+            </Container>
         );
     }
 }
