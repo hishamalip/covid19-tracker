f31418342fe68f87634c76a5bdc45f8b5f841f12
hishamalip <hishamalip@gmail.com>
2020-10-06 20:57:14 +0530

    Updates

---
+++ precache-manifest.f37850937b87baee3050fd84bc559337.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70f6844a222fb8886a686e86f649e921",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "7f725031a9e26b79dc38",
+    "url": "/covid19-tracker/static/css/2.43079d8c.chunk.css"
+  },
+  {
+    "revision": "a3cb4a32b82656e141ee",
+    "url": "/covid19-tracker/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7f725031a9e26b79dc38",
+    "url": "/covid19-tracker/static/js/2.f76d1c1e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/covid19-tracker/static/js/2.f76d1c1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3cb4a32b82656e141ee",
+    "url": "/covid19-tracker/static/js/main.63e6b120.chunk.js"
+  },
+  {
+    "revision": "6b444aef87d753a0bf04",
+    "url": "/covid19-tracker/static/js/runtime-main.815270a9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid19-tracker/precache-manifest.cda5a5377996213d9f5e1402e1665e35.js"
+  "/covid19-tracker/precache-manifest.f37850937b87baee3050fd84bc559337.js"
 );
 
 self.addEventListener('message', (event) => {
